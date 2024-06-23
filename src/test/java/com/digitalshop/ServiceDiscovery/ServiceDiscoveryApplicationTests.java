2324118ca4eaf15887ee6d8f5f0a1f294fd338ca
mansi <mansi.valani@gmail.com>
2024-06-23 05:39:04 +0530

    Implemented Service Discovery MS

---
+++ src/test/java/com/digitalshop/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalshop.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
